a7c3a72ba1e432a5350bf0bdb91512e6eee1d866
Winston Tsang <rubyconsumer@rubyconsumer.com>
2009-12-22 16:38:45 -0500

    Allow Summon::Service.search to recieve and pass on locale information.

--- lib/summon/service.rb
+++ lib/summon/service.rb
@@ -2,15 +2,14 @@
 module Summon
   class Service
     
-    attr_reader :transport, :url, :access_id, :client_key, :locale, :default_locale
+    attr_reader :transport, :url, :access_id, :client_key, :locale
     
     def initialize(options = {})      
       @url        = options[:url] || "http://api.summon.serialssolutions.com"
       @access_id  = options[:access_id]
       @secret_key = options[:secret_key]
       @client_key = options[:client_key]
-      @default_locale = 'en'
-      @locale     = options[:locale] || @default_locale
+      @locale     = options[:locale] || Summon::DEFAULT_LOCALE
       @log        = Log.new(options[:log])
       @transport  = options[:transport] || Summon::Transport::Http.new(:url => @url, :access_id => @access_id, :secret_key => @secret_key, :client_key => @client_key, :session_id => options[:session_id], :log => @log)
     end
@@ -20,8 +19,10 @@ module Summon
     end    
     
     def search(params = {})
+      locale = params['locale'] ? params['locale'] : @locale
+      
       connect("/search", params) do |result|
-        Summon::Search.new(result, @locale)
+        Summon::Search.new(result, locale)
       end
     end
 
@@ -39,7 +40,6 @@ module Summon
     def connect(path, params = {})
       yield @transport.get(path, params)
     end
-            
   end
 end
 
